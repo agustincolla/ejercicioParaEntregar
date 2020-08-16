b79817e4a80950adbc6b2b02764a15841e6af59e
agucoyote <agu_coyote@hotmail.com>
2020-08-15 21:02:05 -0300

    se empezo a agregar funcionalidades para tomar datos desde una base

--- demo/src/main/java/com/example/demo/dao/PostgresRoleDao.java
+++ demo/src/main/java/com/example/demo/dao/PostgresRoleDao.java
@@ -4,12 +4,21 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Role;
 
 @Repository("postgres")
 public class PostgresRoleDao implements RoleDao{
+
+	private final JdbcTemplate jdbcTemplate;
+	@Autowired
+	public PostgresRoleDao(JdbcTemplate jdbcTemplate) {
+		super();
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public int insertRole(UUID id, Role role) {
@@ -18,8 +27,15 @@ public class PostgresRoleDao implements RoleDao{
 	}
 
 	@Override
+	//return all role
 	public List<Role> selectAllRole() {
 		// TODO Auto-generated method stub
+		final String query="SELECT id , roleName FROM role";
+		jdbcTemplate.query(query,(resultSet,i)->{
+			UUID id=UUID.fromString(resultSet.getString("id"));
+			String roleName=resultSet.getString("roleName");
+			return new Role(id,roleName);
+			});
 		return  List.of(new Role(UUID.randomUUID(),"FROM POSTGRES DB"));
 	}
 
