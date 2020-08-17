469578fa4433c3ba0c4e09dafa74fbd91da00351
agucoyote <agu_coyote@hotmail.com>
2020-08-17 18:10:13 -0300

    se agrego la base para que empiece con 3 roles y un usuario con esos 3 roles

--- demo/src/main/resources/db/migration/V1__RoleTable.sql
+++ demo/src/main/resources/db/migration/V1__RoleTable.sql
@@ -1,4 +1,22 @@
-CREATE TABLE role(
-    id UUID NOT NULL PRIMARY KEY,
+CREATE TABLE role (
+    roleId UUID NOT NULL PRIMARY KEY,
     roleName VARCHAR(100) NOT NULL
+); 
+CREATE TABLE person(
+   personId UUID NOT NULL PRIMARY KEY,
+   personEmail VARCHAR(100) NOT NULL ,
+   personFirstName VARCHAR(100) NOT NULL, 
+   personLastName VARCHAR(100) NOT NULL
 );
+CREATE TABLE personRole (
+  personRoleId UUID NOT NULL PRIMARY KEY,
+  personID UUID NOT NULL,
+  roleId UUID NOT NULL 
+);
+INSERT INTO role (roleId,roleName) VALUES (uuid_generate_v4(),'admin');
+INSERT INTO role (roleId,roleName) VALUES (uuid_generate_v4(),'gerente');
+INSERT INTO role (roleId,roleName) VALUES (uuid_generate_v4(),'técnico');
+INSERT INTO person (personId,personEmail,personFirstName,personLastName) VALUES (uuid_generate_v4(),'agu_coyote@hotmail.com','Agustin','Colla');
+INSERT INTO personRole (personRoleId,personId,roleId) VALUES (uuid_generate_v4(),(SELECT personId FROM person WHERE personEmail='agu_coyote@hotmail.com'),(SELECT roleId FROM role WHERE roleName='admin'));
+INSERT INTO personRole (personRoleId,personId,roleId) VALUES (uuid_generate_v4(),(SELECT personId FROM person WHERE personEmail='agu_coyote@hotmail.com'),(SELECT roleId FROM role WHERE roleName='gerente'));
+INSERT INTO personRole (personRoleId,personId,roleId) VALUES (uuid_generate_v4(),(SELECT personId FROM person WHERE personEmail='agu_coyote@hotmail.com'),(SELECT roleId FROM role WHERE roleName='técnico'));
